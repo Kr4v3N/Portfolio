961cc0c4f19e3f6d133117cccf30d5dc9b0d0acd
Kr4v3N <fafachen37@gmail.com>
2019-01-14 16:50:50 +0100

    Adding htaccess file

--- js/app.js
+++ js/app.js
@@ -1,27 +1,24 @@
 $(function(){
 
     $(".navbar a, footer a").on("click", function(e){
-
         e.preventDefault();
         var hash = this.hash;
-
-        $("body,html").animate({scrollTop: $(hash).offset().top} , 1000 , function(){window.location.hash = hash;});
-
+        $("body,html").animate({scrollTop: $(hash).offset().top} , 1300 , function(){window.location.hash = hash;});
     });
 
     $('#contact-form').submit(function(e) {
     e.preventDefault();
     $('.comments').empty();
     var postdata = $('#contact-form').serialize();
-    
+
         $.ajax({
             type: 'POST',
             url: '/php/contact.php',
             data: postdata,
             dataType: 'json',
             success: function(json) {
-                 
-                if(json.isSuccess) 
+
+                if(json.isSuccess)
                 {
                     $('#contact-form').append("<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté :)</p>");
                     $('#contact-form')[0].reset();
@@ -33,7 +30,7 @@ $(function(){
                     $('#email + .comments').html(json.emailError);
                     $('#phone + .comments').html(json.phoneError);
                     $('#message + .comments').html(json.messageError);
-                }                
+                }
             }
         });
     });
