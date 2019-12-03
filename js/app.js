b7e5921ecb0930d82c619a392b1b5c33b90304b2
Kr4v3n <fafachen37@gmail.com>
2019-12-03 17:59:21 +0100

    Add hover

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(function()
 
     $(".navbar a, footer a").on("click", function(e){
         e.preventDefault();
-        var hash = this.hash;
+        let hash = this.hash;
         $("body,html").animate({scrollTop: $(hash).offset().top} , 1300 , function(){window.location.hash = hash;});
     });
 
@@ -21,7 +21,7 @@ $(function()
 
                 if(json.isSuccess)
                 {
-                    $('#contact-form').append("<p class='thank-you alert alert-success'>Votre message a bien été envoyé. Merci de m'avoir contacté :)</p>");
+                    $('#contact-form').append("<p class='thank-you alert alert-success'>Votre message a bien été envoyé.)</p>");
                     $('#contact-form')[0].reset();
                 }
                 else
